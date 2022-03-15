99f1617949ffc0b7ff990d39930e25c3a00988e8
Abenezer Ephrem <99406488+Abenieph@users.noreply.github.com>
2022-03-15 10:53:57 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf('_');
+	printf('p');
+	printf('u');
+	printf('t');
+	printf('c');
+	printf('h');
+	printf('a');
+	printf('r');
+	printf('\n');
 
 	return (0);
 }
